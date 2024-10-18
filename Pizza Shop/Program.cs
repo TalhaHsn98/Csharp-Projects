b6b609e124c41078530233034b40cee471718ff3
Talha Hussain Syed <syedtalha.hussain98@gmail.com>
2024-10-18 07:56:41 -0400

    Starting Now

---
+++ Pizza Shop/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pizza_Shop
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter your name");
+
+        }
+
+    }
+
+}
