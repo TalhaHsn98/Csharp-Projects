77a9b7a2b465311f68c55af3a7865279fa3d661c
Talha Hussain Syed <syedtalha.hussain98@gmail.com>
2024-10-29 23:31:52 -0400

    New Code

---
+++ Inventory Management/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management
+{
+    internal class Product
+    {
+        //begin to write the code
+    }
+}
